41268e01f47815dc9245f99bdc25e8cbb595d02e
sdsd34343sdfd33434343444445454dfdfdfsdf <neelam.chavan@smartbear.com>
2025-11-25 11:44:42 +0530

    Modified 783 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzMDAzMA/spectral.js?branch_slug=master&token=3397300e-58ab-4ffa-a7b2-86ad4671e812";
+export default { extends: ruleset };
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# new1111
+# api-documentation
+
+This project must be edited via Stoplight Studio. Visit https://skaleet.stoplight.io/ with valid credentials to edit the documentation content.
